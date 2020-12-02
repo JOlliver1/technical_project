017e0780c5861b462d944df22577d2435590d9a5
James <jamesolliver@hotmail.co.uk>
2020-12-02 15:30:06 +0000

    extended the code to model an infectious disease

--- TP_random.py
+++ TP_random.py
@@ -18,16 +18,23 @@ class Agent(Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.knowledge = 0
+        self.infected = 0
 
     def spread_news(self):
-        if self.knowledge == 0:
+        if self.infected == 0:
             return
         neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=True)
         neig_agents = [a for n in neighbors for a in self.model.grid.get_cell_list_contents(n.pos)]
         for a in neig_agents:
-            if random.random() < 0.3:
-                a.knowledge = 1
+            if i < 80:
+                if random.random() < 0.03:
+                    a.infected = 1
+            if i > 160:
+                if random.random() < 0.05:
+                    a.infected = 1
+            else:
+                if random.random() < 0.005:
+                    a.infected = 1
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
@@ -46,13 +53,13 @@ class Agent(Agent):
 # are aware of the product.
 
 def compute_informed(model):
-    return sum([1 for a in model.schedule.agents if a.knowledge == 1])
+    return sum([1 for a in model.schedule.agents if a.infected == 1])
 
 # now let's define the model
 
 
 class News_Model(Model):
-    def __init__(self, N, width, height):
+    def __init__(self, N, width, height, initial):
         self.num_agents = N
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
@@ -65,14 +72,12 @@ class News_Model(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
-            l = [1, 2, 3, 4, 5]
-            # 5 agents are aware of the product
-            if i in l:  # only agents with id in the list are aware of the product
-                a.knowledge = 1
+            if i < initial:
+                a.infected = 1
 
         self.datacollector = DataCollector(
             model_reporters={"Tot informed": compute_informed},
-            agent_reporters={"Knowledge": "knowledge"})
+            agent_reporters={"Infected": "infected"})
 
     def step(self):
         self.datacollector.collect(self)
@@ -80,15 +85,30 @@ class News_Model(Model):
 
 
 '''Run the model '''
-model = News_Model(100, 50, 50)
-for i in range(100):
+model = News_Model(N=750,
+                   width=60,
+                   height=60,
+                   initial=1)
+
+steps = 200
+for i in range(steps):
     model.step()
 
 # let's inspect the results:
 out = model.datacollector.get_agent_vars_dataframe().groupby('Step').sum()
 new_out = out.to_numpy()
-#print(type(new_out[:]))
+print(new_out)
+print(np.diff(new_out.T))
 
-plt.plot(np.arange(0, 100), new_out[:])
+plt.subplot(211)
+plt.plot(np.arange(0, steps), new_out, 'o')
+plt.xlabel('Steps')
+plt.ylabel('No. of People')
+plt.grid()
+plt.subplot(212)
+plt.plot(np.arange(0, steps-1), np.diff(new_out.T).T, 'x')
+plt.xlabel('Steps')
+plt.ylabel('No. of New People')
+plt.grid()
 plt.show()
 
